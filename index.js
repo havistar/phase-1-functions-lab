a7d6d29236fe37bbd2a81d12a208a63f28ecf874
Christina Nguyen <cnguyen4290@yahoo.com>
2021-08-12 00:53:21 -0500

    commit tree 0a6b3933c8224181238ef74ee377b32b32e2bd29

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const hq = 42
+const feet = 264
+
+function distanceFromHqInBlocks(destination){
+  if (destination > 42){return destination - hq}
+  else if (destination < 42) {return hq - destination}
+ }
+
+ function distanceFromHqInFeet(destination){
+   return distanceFromHqInBlocks(destination)*feet
+ }
+
+ function distanceTravelledInFeet(start, end) {
+    const destination = Math.abs(start-end)
+    return destination * 264;
+ }
+ 
+ function calculatesFarePrice (start, destination) {
+    const traveled = distanceTravelledInFeet(start, destination);
+    if (traveled > 400 && traveled < 2000){
+            console.log(traveled)
+            return (traveled - 400) * .02
+      } else if (traveled > 2000 && traveled < 2500) {
+        return 25;
+      } else if (traveled < 400) {
+        return 0;
+      } else if (traveled > 2500) {
+        return "cannot travel that far";
+      }
+  }
